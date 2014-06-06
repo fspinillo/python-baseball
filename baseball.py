a0c97f1200807c5c0a50a92d9d8f93daa9bede9f
fspinillo <frankspinillo@gmail.com>
2014-06-05 20:54:44 -0400

    Big changes with library and feed
    Switched from urllib2 to requests
    Switched the feed to be able to pull in more data
    Procedures have been expanded to check for game status
    If the team is defined, different info is displayed pending game status

--- baseball.py
+++ baseball.py
@@ -1,59 +1,61 @@
 import datetime
-import urllib2
+import requests
 import json
 
-# Procedure checks the team input for scores first.
-# If scores is true, then it prints info for all the games.
-# If a team abbreviation is true, it prints the game info only for that game.
+def game_info():
+    if game['status']['status'] == "In Progress":
+        return '%s (%s) vs %s (%s) @ %s %s' % (game['away_team_name'], game['linescore']['r']['away'], game['home_team_name'], \
+            game['linescore']['r']['home'], game['venue'], game['status']['status'])
+    elif game['status']['status'] == "Final":
+        return '%s (%s) vs %s (%s) @ %s %s' % (game['away_team_name'], game['linescore']['r']['away'], game['home_team_name'], \
+            game['linescore']['r']['home'], game['venue'], game['status']['status'])
+    elif (game['status']['status'] == "Pre-Game" or game['status']['status'] == "Preview"):
+        return '%s vs %s @ %s %s %s' % (game['away_team_name'], game['home_team_name'], \
+            game['venue'], game['time'], game['status']['status'])
 
-def team_score(team):
-    if team == 'SCORES':
-        return '%s (%s) vs %s (%s) @ %s' % (game['away_team_name'], game['away_team_runs'], \
-                game['home_team_name'], game['home_team_runs'], game['venue'])
-    elif (game['home_name_abbrev']) == team or (game['away_name_abbrev']) == team:
-            return '%s (%s) vs %s (%s) @ %s' % (game['away_team_name'], game['away_team_runs'], \
-                game['home_team_name'], game['home_team_runs'], game['venue'])
-    else:
-        return False
-
-# Procedure checks the same as above, except this is used if the game has not started yet.
-
-def team_pending(team):
-    if team == 'SCORES':
-        return '%s vs %s @ %s %s' % (game['away_team_name'], game['home_team_name'], game['venue'], game['time'])
-    elif (game['home_name_abbrev']) == team or (game['away_name_abbrev']) == team:
-        return '%s vs %s @ %s %s' % (game['away_team_name'], game['home_team_name'], game['venue'], game['time'])
-    else:
-        return False
-
-# Requests the type of score information from the user.
-
-print "Please enter the name of the team you want scores for.", '\n' \
-        "ex: NYY, SFG, LAD, etc.", '\n' \
-        "If you just want all the scores, enter scores."
-
-team = raw_input('Which team shall it be?: ').upper()
-
-# Captures the current date, and appends it to the base url for the data.
+def team_score():
+    if game['status']['status'] == "In Progress":
+        return '-------------------------------\n' \
+        '%s (%s) vs. %s (%s) @ %s\n' \
+        '%s: %s of the %s\n' \
+        'Pitching: %s || Batting: %s || S: %s B: %s O: %s\n' \
+        '-------------------------------' % (game['away_team_name'], game['linescore']['r']['away'], \
+            game['home_team_name'], game['linescore']['r']['home'], game['venue'], game['status']['status'], \
+            game['status']['inning_state'], game['status']['inning'], game['pitcher']['last'], game['batter']['last'], \
+            game['status']['s'], game['status']['b'], game['status']['o'])
+    elif game['status']['status'] == "Final":
+        return '-------------------------------\n' \
+        '%s (%s) vs. %s (%s) @ %s\n' \
+        'W: %s || L: %s || SV: %s\n' \
+        '-------------------------------' % (game['away_team_name'], game['linescore']['r']['away'], \
+            game['home_team_name'], game['linescore']['r']['home'], game['venue'], \
+            game['winning_pitcher']['name_display_roster'], game['losing_pitcher']['name_display_roster'], \
+            game['save_pitcher']['name_display_roster'])
+    elif (game['status']['status'] == "Pre-Game" or game['status']['status'] == "Preview"):
+        return '-------------------------------\n' \
+        '%s vs %s @ %s %s\n' \
+        'P: %s || P: %s\n' \
+        '-------------------------------' % (game['away_team_name'], game['home_team_name'], game['venue'], \
+            game['time'], game['away_probable_pitcher']['name_display_roster'], \
+            game['home_probable_pitcher']['name_display_roster'])
 
 now = datetime.datetime.now()
-gameday_url = "http://gd2.mlb.com/components/game/mlb/year_%d/month_%s/day_%s/miniscoreboard.json" \
+baseball_url = "http://gd2.mlb.com/components/game/mlb/year_%d/month_%s/day_%s/master_scoreboard.json" \
                 % (now.year, now.strftime("%m"), now.strftime("%d"))
 
-# Pulls the json info into urllib2 and creates the array for the procedures
+baseball_data = requests.get(baseball_url)
+game_data = baseball_data.json()
+game_array = game_data['data']['games']['game']
 
-gameday_response = urllib2.urlopen(gameday_url)
-gameday_data = json.load(gameday_response)
-games_array = gameday_data['data']['games']['game']
+team = raw_input('What team do you want? ex. SF or NYY ').upper()
 
-#calls the procedures based on if the game has started or not
-
-for game in games_array:
-    if 'home_team_runs' in game:
-        disp = team_score(team)
+for game in game_array:
+    if team == "":
+        disp = game_info()
         if disp:
             print disp
     else:
-        disp = team_pending(team)
-        if disp:
-            print disp
+        if (game['home_name_abbrev']) == team or (game['away_name_abbrev']) == team:
+            disp = team_score()
+            if disp:
+                print disp
